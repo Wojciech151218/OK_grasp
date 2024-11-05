c97fb9475b9894a1d95cf1ae26c2ccaf7544468b
wojciech <wojciech@dbc>
2024-11-05 22:30:10 +0100

    update

---
+++ cmake-build-debug/CMakeFiles/OK_grasp.dir/DependInfo.cmake
@@ -0,0 +1,28 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/home/wojciech/CLionProjects/OK_grasp/DataLoader.cpp" "CMakeFiles/OK_grasp.dir/DataLoader.cpp.o" "gcc" "CMakeFiles/OK_grasp.dir/DataLoader.cpp.o.d"
+  "/home/wojciech/CLionProjects/OK_grasp/DataPoint.cpp" "CMakeFiles/OK_grasp.dir/DataPoint.cpp.o" "gcc" "CMakeFiles/OK_grasp.dir/DataPoint.cpp.o.d"
+  "/home/wojciech/CLionProjects/OK_grasp/Graph.cpp" "CMakeFiles/OK_grasp.dir/Graph.cpp.o" "gcc" "CMakeFiles/OK_grasp.dir/Graph.cpp.o.d"
+  "/home/wojciech/CLionProjects/OK_grasp/Problem.cpp" "CMakeFiles/OK_grasp.dir/Problem.cpp.o" "gcc" "CMakeFiles/OK_grasp.dir/Problem.cpp.o.d"
+  "/home/wojciech/CLionProjects/OK_grasp/Solution.cpp" "CMakeFiles/OK_grasp.dir/Solution.cpp.o" "gcc" "CMakeFiles/OK_grasp.dir/Solution.cpp.o.d"
+  "/home/wojciech/CLionProjects/OK_grasp/main.cpp" "CMakeFiles/OK_grasp.dir/main.cpp.o" "gcc" "CMakeFiles/OK_grasp.dir/main.cpp.o.d"
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_LINKED_INFO_FILES
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_FORWARD_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ cmake-build-debug/CMakeFiles/OK_grasp.dir/cmake_clean.cmake
@@ -0,0 +1,21 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/OK_grasp.dir/DataLoader.cpp.o"
+  "CMakeFiles/OK_grasp.dir/DataLoader.cpp.o.d"
+  "CMakeFiles/OK_grasp.dir/DataPoint.cpp.o"
+  "CMakeFiles/OK_grasp.dir/DataPoint.cpp.o.d"
+  "CMakeFiles/OK_grasp.dir/Graph.cpp.o"
+  "CMakeFiles/OK_grasp.dir/Graph.cpp.o.d"
+  "CMakeFiles/OK_grasp.dir/Problem.cpp.o"
+  "CMakeFiles/OK_grasp.dir/Problem.cpp.o.d"
+  "CMakeFiles/OK_grasp.dir/Solution.cpp.o"
+  "CMakeFiles/OK_grasp.dir/Solution.cpp.o.d"
+  "CMakeFiles/OK_grasp.dir/main.cpp.o"
+  "CMakeFiles/OK_grasp.dir/main.cpp.o.d"
+  "OK_grasp"
+  "OK_grasp.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/OK_grasp.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
