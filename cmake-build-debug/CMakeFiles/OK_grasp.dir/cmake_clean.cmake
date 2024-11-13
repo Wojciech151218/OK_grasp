29f5f72fa1154c1f0f18d19f561e476f131bdc61
krzys <krzysztofzaporowski@gmail.com>
2024-11-13 15:33:57 +0100

    Load solution

---
+++ cmake-build-debug/CMakeFiles/OK_grasp.dir/cmake_clean.cmake
@@ -0,0 +1,25 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/OK_grasp.dir/DataLoader.cpp.obj"
+  "CMakeFiles/OK_grasp.dir/DataLoader.cpp.obj.d"
+  "CMakeFiles/OK_grasp.dir/DataPoint.cpp.obj"
+  "CMakeFiles/OK_grasp.dir/DataPoint.cpp.obj.d"
+  "CMakeFiles/OK_grasp.dir/Graph.cpp.obj"
+  "CMakeFiles/OK_grasp.dir/Graph.cpp.obj.d"
+  "CMakeFiles/OK_grasp.dir/Problem.cpp.obj"
+  "CMakeFiles/OK_grasp.dir/Problem.cpp.obj.d"
+  "CMakeFiles/OK_grasp.dir/Solution.cpp.obj"
+  "CMakeFiles/OK_grasp.dir/Solution.cpp.obj.d"
+  "CMakeFiles/OK_grasp.dir/main.cpp.obj"
+  "CMakeFiles/OK_grasp.dir/main.cpp.obj.d"
+  "CMakeFiles/OK_grasp.dir/utils.cpp.obj"
+  "CMakeFiles/OK_grasp.dir/utils.cpp.obj.d"
+  "OK_grasp.exe"
+  "OK_grasp.exe.manifest"
+  "OK_grasp.pdb"
+  "libOK_grasp.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/OK_grasp.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
